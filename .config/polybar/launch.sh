12337e0a567bc0e9f58df5801c82f84f76ca28cc
Sam - laptop <samuel.grulich@gmail.com>
2024-10-12 23:55:25 +0200

    Laptop fixes + removed old files
    changed keyboard group settings
    changed polybar launch script to account for one monitor
    removed old polybar files
    changed polybar baterry icons

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,12 +8,18 @@ killall -q polybar
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 # polybar bar 2>&1 | tee -a /tmp/polybar.log & diown
 
+
+MONITORS=$(xrandr --query)
 BAR_NAME=bar
 BAR_CONFIG=/home/$USER/.config/polybar/config.ini
 
-PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
-OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+PRIMARY=$($MONITORS | grep " connected" | grep    "primary" | cut -d" " -f1)
+OTHERS=$($MONITORS  | grep " connected" | grep -v "primary" | cut -d" " -f1)
 
+if [[ ${#MONITORS[@]} -eq 1 ]]; then
+    polybar --reload -c "$BAR_CONFIG" $BAR_NAME &
+    exit
+fi
 # Launch on primary monitor
 MONITOR=$PRIMARY polybar --reload -c "$BAR_CONFIG" $BAR_NAME &
 sleep 1
@@ -22,15 +28,4 @@ sleep 1
 for m in $OTHERS; do
     MONITOR=$m polybar --reload -c "$BAR_CONFIG" $BAR_NAME &
 done
-
-# if type "xrandr"; then
-#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#     echo m
-#     MONITOR=$m polybar --reload bar 2>&1 | tee -a /tmp/polybar.log &
-#   done
-# else
-#   polybar --reload bar &
-# fi
-
-echo "Polybar launched..."
 
