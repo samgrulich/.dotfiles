917548a0827f1d3be12fded1e0ff407032350ac4
Sam <samuel.grulich@gmail.com>
2025-03-10 22:05:49 +0100

    Changed configs a bit

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -11,19 +11,4 @@ return {
 			indent = { char = { "│", "╎" }, tab_char = { "│", "╎" } },
 		},
 	},
-	{
-		"goolord/alpha-nvim",
-		-- dependencies = { 'echasnovski/mini.icons' },
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			local startify = require("alpha.themes.startify")
-			-- available: devicons, mini, default is mini
-			-- if provider not loaded and enabled is true, it will try to use another provider
-			startify.file_icons.provider = "devicons"
-			require("alpha").setup(startify.config)
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-	},
 }
