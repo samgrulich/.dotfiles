370008697fa054ee622d1b727dc2ab39a5c2b4ae
Sam <samuel.grulich@gmail.com>
2025-03-15 21:52:15 +0100

    Basic working setup (lsp + autocomplete)
    added lsp support
    added autocomplete support
    added formatting list
    added spacing by format setting

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,46 +1,67 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        dependencies = { "mason.nvim" },
-        lazy = true,
-        event = "BufWritePre",
-        cmd = "ConformInfo",
-        opts = function()
-            ---@type conform.setupOpts
-            local opts = {
-                default_format_opts = {
-                    timeout_ms = 3000,
-                    async = false, -- not recommended to change
-                    quiet = false, -- not recommended to change
-                    lsp_format = "fallback", -- not recommended to change
-                },
-                formatters_by_ft = {
-                    lua = { "stylua" },
-                    python = { "black" },
-                },
-                format_on_save = {
-                    timeout_ms = 500,
-                    lsp_format = "fallback",
-                },
-                -- The options you set here will be merged with the builtin formatters.
-                -- You can also define any custom formatters here.
-                ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
-                formatters = {
-                    injected = { options = { ignore_errors = true } },
-                    -- # Example of using dprint only when a dprint.json file is present
-                    -- dprint = {
-                    --   condition = function(ctx)
-                    --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-                    --   end,
-                    -- },
-                    --
-                    -- # Example of using shfmt with extra args
-                    -- shfmt = {
-                    --   prepend_args = { "-i", "2", "-ci" },
-                    -- },
-                },
-            }
-            return opts
-        end,
-    },
+	{
+		"stevearc/conform.nvim",
+		dependencies = { "mason.nvim" },
+		lazy = true,
+		event = "VeryLazy",
+		cmd = "ConformInfo",
+		opts = function()
+			---@type conform.setupOpts
+			local opts = {
+				default_format_opts = {
+					timeout_ms = 3000,
+					async = false, -- not recommended to change
+					quiet = false, -- not recommended to change
+					lsp_format = "fallback", -- not recommended to change
+				},
+				formatters_by_ft = {
+					lua = { "stylua" },
+					python = { "black" },
+					bash = { "shfmt" },
+					sh = { "shfmt" },
+					fish = { "fish_indent" },
+					go = { "goimports", "gofumpt", "goimports-reviser" },
+					javascript = { "prettierd", "prettier", stop_after_first = true },
+					typescript = { "prettierd", "prettier", stop_after_first = true },
+					javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+					typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+					svelte = { "prettierd", "prettier", stop_after_first = true },
+					vue = { "prettierd", "prettier", stop_after_first = true },
+					css = { "prettierd", "prettier", stop_after_first = true },
+					scss = { "prettierd", "prettier", stop_after_first = true },
+					less = { "prettierd", "prettier", stop_after_first = true },
+					html = { "prettierd", "prettier", stop_after_first = true },
+					json = { "prettierd", "prettier", stop_after_first = true },
+					jsonc = { "prettierd", "prettier", stop_after_first = true },
+					yaml = { "prettierd", "prettier", stop_after_first = true },
+					markdown = { "prettierd", "prettier", stop_after_first = true },
+					["markdown.mdx"] = { "prettierd", "prettier", stop_after_first = true },
+					graphql = { "prettierd", "prettier", stop_after_first = true },
+					handlebars = { "prettierd", "prettier", stop_after_first = true },
+				},
+				format_on_save = {
+					-- timeout_ms = 500,
+					lsp_format = "fallback",
+				},
+				-- The options you set here will be merged with the builtin formatters.
+				-- You can also define any custom formatters here.
+				---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+				formatters = {
+					injected = { options = { ignore_errors = true } },
+					-- # Example of using dprint only when a dprint.json file is present
+					-- dprint = {
+					--   condition = function(ctx)
+					--     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+					--   end,
+					-- },
+					--
+					-- # Example of using shfmt with extra args
+					-- shfmt = {
+					--   prepend_args = { "-i", "2", "-ci" },
+					-- },
+				},
+			}
+			return opts
+		end,
+	},
 }
---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,54 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"saghen/blink.cmp",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			{
+				"folke/lazydev.nvim",
+				opts = {
+					library = {
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+		},
+		config = function()
+			-- insert new servers here + config
+			-- for the complete list use :h lspconfig-all
+			local servers = {
+				lua_ls = {},
+				pyright = {},
+				clangd = {},
+				svelte = {},
+				tailwindcss = {},
+				eslint = {},
+				ts_ls = {},
+			}
+
+			require("mason").setup()
+
+			local ensure_installed = vim.tbl_keys(servers or {})
+
+			require("mason-lspconfig").setup({ ensure_installed = ensure_installed, automatic_installation = false })
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+			require("mason-lspconfig").setup_handlers({
+				function(server_name)
+					local server = servers[server_name] or {}
+					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+					require("lspconfig")[server_name].setup(server)
+				end,
+			})
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local c = vim.lsp.get_client_by_id(args.data.client_id)
+					if not c then
+						return
+					end
+				end,
+			})
+		end,
+	},
+}
